59f07ba9ae5f2517b3709dc04d96e3bc0075a199
Andrei Monichev <vyroud@gmail.com>
2024-12-27 19:58:23 +0300

    restructured folders

--- conftest.py
+++ conftest.py
@@ -3,18 +3,29 @@ from selene import browser
 
 
 @pytest.fixture(params=[(1920, 1080)])
-def desktop_version(request):
+def desktop_resolution(request):
     width, height = request.param
     browser.config.window_width = width
     browser.config.window_height = height
     yield
     browser.quit()
 
-
 @pytest.fixture(params=[(1000, 1080)])
-def mobile_version(request):
+def mobile_resolution(request):
     width, height = request.param
     browser.config.window_width = width
     browser.config.window_height = height
     yield
+    browser.quit()
+
+@pytest.fixture(params=[(1920, 1080), (1000, 1080)])
+def setup_browser(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    if width > 1012:
+        yield "desktop"
+    else:
+        yield "mobile"
+
     browser.quit()
--- test_github_indirect.py
+++ test_github_indirect.py
@@ -2,14 +2,14 @@ import pytest
 from selene import browser, by
 
 
-@pytest.mark.parametrize("desktop_version", [(1650, 1050)], indirect=True)
-def test_desktop(desktop_version):
+@pytest.mark.parametrize("desktop_resolution", [(1650, 1050)], indirect=True)
+def test_desktop(desktop_resolution):
     browser.open("http://github.com")
     browser.element(by.text("Sign up")).click()
 
 
-@pytest.mark.parametrize("mobile_version", [(900, 1000)], indirect=True)
-def test_mobile(mobile_version):
+@pytest.mark.parametrize("mobile_resolution", [(900, 1000)], indirect=True)
+def test_mobile(mobile_resolution):
     browser.open("http://github.com")
     browser.element("[class='Button-content']").click()
     browser.element(by.text("Sign up")).click()
